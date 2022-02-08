b4fb4caf92c5f15235ab9ef468f50a208bb685cf
avkatlant <avkatlant@gmail.com>
2022-02-08 20:00:30 +0700

    clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user.component';
-import { ProductComponent } from './components/product/product.component';
-import { CategotyComponent } from './categoty/categoty.component';
-import { ReviewComponent } from './components/review/review.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    ProductComponent,
-    CategotyComponent,
-    ReviewComponent
+
   ],
   imports: [
     BrowserModule,
