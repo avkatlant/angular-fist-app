07ebab8524b0433aa7cd675256f6ce7ce8ed9754
avkatlant <avkatlant@gmail.com>
2022-02-08 19:57:51 +0700

    Все способы создания компонента. Декоратор @Component

---
+++ src/app/categoty/categoty.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categoty',
+  templateUrl: './categoty.component.html',
+  styleUrls: ['./categoty.component.scss']
+})
+export class CategotyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  template:
+    `
+      <div>Hello I am UserComponenr</div>
+    `
+})
+
+export class UserComponent {
+}
